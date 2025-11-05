b4c2427e756db04eb982e5d176caf270023a1f91
Irfan Susilo <askirfan94@gmail.com>
2025-11-05 11:47:03 +0700

    unit test : post detail

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -36,4 +36,66 @@ class PostTest extends TestCase
             ->assertStatus(200)
             ->assertJson([]);
     }
+
+    /**
+     * detail post testing
+     */
+    public function test_post_detail_is_accessible_for_guests(): void
+    {
+        $user = User::factory()->create();
+        $post = Post::factory()->create([
+            'user_id' => $user->id,
+            'title' => $this->faker->sentence,
+            'is_draft' => 0,
+            'content' => $this->faker->paragraph,
+        ]);
+
+        $this->getJson("/posts/{$post->id}")
+            ->assertStatus(201)
+            ->assertJson([]);
+    }
+
+    public function test_post_detail_is_accessible_for_authenticated_user(): void
+    {
+        $user = User::factory()->create();
+        $post = Post::factory()->create([
+            'user_id' => $user->id,
+            'title' => $this->faker->sentence,
+            'is_draft' => 0,
+            'content' => $this->faker->paragraph,
+        ]);
+
+        $this->actingAs($user)
+            ->getJson("/posts/{$post->id}")
+            ->assertStatus(201)
+            ->assertJson([]);
+    }
+
+    public function test_post_detail_should_return_404_if_it_is_a_draft_post(): void
+    {
+        $user = User::factory()->create();
+        $draftPost = Post::factory()->create([
+            'user_id' => $user->id,
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+            'is_draft' => 1,
+        ]);
+
+        $this->getJson("/posts/{$draftPost->id}")
+            ->assertStatus(404);
+    }
+
+    public function test_post_detail_should_return_201_if_it_is_an_active_post(): void
+    {
+        $user = User::factory()->create();
+        $draftPost = Post::factory()->create([
+            'user_id' => $user->id,
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+            'is_draft' => 0,
+        ]);
+
+        $this->getJson("/posts/{$draftPost->id}")
+            ->assertStatus(201);
+    }
 }
